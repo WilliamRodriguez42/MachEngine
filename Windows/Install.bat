464e62321bd818dd8e6bd8f9df1e79781bce4b99
warjr0000 <warjr0000@hotmail.com>
2018-09-25 15:32:56 -0400

    Changed installer to not automatically update pip
    Also added extra dependencies

--- Windows/Install.bat
+++ Windows/Install.bat
@@ -3,8 +3,7 @@
 setlocal
 cd /d %~dp0
 
-python -m pip install --upgrade pip
-pip install PyOpenGL PyQt5 numpy pillow lxml
+pip install PyOpenGL PyQt5 numpy pillow lxml libaudioverse
 
 cd ..\pyinstaller-develop\
 
